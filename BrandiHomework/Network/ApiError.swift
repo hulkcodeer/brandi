b29317e577fd658c0bd5834e21675049804915e7
hyunjin.park <unicon85@gmail.com>
2022-03-23 13:26:28 +0900

    :construction: API 추가

--- BrandiHomework/Network/ApiError.swift
+++ BrandiHomework/Network/ApiError.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import RxSwift
+
+internal enum ApiError {
+    case responseError(error: Enums.NetworkError)
+    case otherServerError(data: Data)
+    
+    class Enums {}
+}
+
+extension ApiError: LocalizedError {
+    internal var errorDescription: String? {
+        switch self {
+        case .responseError(let type): return type.errorDescription
+        case .otherServerError(let data): return data.description
+        }
+    }
+}
+
+// MARK: - Network Errors
+
+extension ApiError.Enums {
+    enum NetworkError {
+        case timeout(url: String, errorCode: Int)
+        case parsing(url: String, statusCode: Int)
+        case other(errorDesc: String, url: String, statusCode: Int, errorCode: Int)
+        case notConnected(url: String, errorCode: Int)
+        case cancelled(url: String, errorCode: Int)
+        case unknown(url: String)
+    }
+}
+
+extension ApiError.Enums.NetworkError: LocalizedError {
+    internal var errorDescription: String? {
+        switch self {
+        case .timeout: return "Request timeout."
+        case .parsing: return "Parsing error."
+        case .other(let errorDesc, _, _, _): return "\(errorDesc)"
+        case .notConnected: return "Device not connected."
+        case .cancelled: return "Cancelled."
+        case .unknown: return "Unknown error occurred."
+        }
+    }
+    
+    internal var errorCode: Int? {
+        switch self {
+        case .timeout(_, let code): return code
+        case .parsing: return 901
+        case .other(_, _, _, let code): return code
+        case .notConnected(_, let code): return code
+        case .cancelled(_, let code): return code
+        case .unknown: return 802
+        }
+    }
+}
--- BrandiHomework/Network/BrandiAPI.swift
+++ BrandiHomework/Network/BrandiAPI.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import RxSwift
+
+protocol BrandiAPI: AnyObject {
+    func fetchList(by model: SearchFilterModel) -> Single<Data>
+}
+
+internal final class RestApi: BrandiAPI {
+ 
+    init() {}
+        
+    func fetchList(by model: SearchFilterModel) -> Single<Data> {
+        let url = "https://dapi.kakao.com/v2/search/image?query=\(model.query.urlEncoded)&sort=\(model.sort.rawValue)&page=\(model.page)&size=\(model.size)"
+        return NetworkWorker.rx.rxRequest((url: url, method: .get, parameters: nil, headers: ["Authorization":"KakaoAK e54ff09acc92305f506e5668a0f6db5a"]))
+    }
+}
